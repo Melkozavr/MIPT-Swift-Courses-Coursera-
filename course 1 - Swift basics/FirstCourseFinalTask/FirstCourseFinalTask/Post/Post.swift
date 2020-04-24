39955b2d81b76f0bbaa3544dfe9df9d67906cfcd
Marat <ghost.marat@gmail.com>
2020-04-24 20:19:46 +0300

    final_task

---
+++ course 1 - Swift basics/FirstCourseFinalTask/FirstCourseFinalTask/Post/Post.swift
@@ -0,0 +1,39 @@
+//
+//  Post.swift
+//  FirstCourseFinalTask
+//
+//  Created by Melkozavr on 23.04.2020.
+//  Copyright © 2020 E-Legion. All rights reserved.
+//
+
+import Foundation
+import FirstCourseFinalTaskChecker
+
+class Post: PostProtocol {
+    
+    var id: PostProtocol.Identifier
+    
+    var author: GenericIdentifier<UserProtocol>
+    
+    var description: String
+    
+    var imageURL: URL
+    
+    var createdTime: Date
+    
+    var currentUserLikesThisPost: Bool
+    
+    var likedByCount: Int
+    
+    init(id: PostProtocol.Identifier, author: GenericIdentifier<UserProtocol>, description: String, imageURL: URL, createdTime: Date, currentUserLikesThisPost: Bool, likedByCount: Int) {
+        
+        self.id = id
+        self.author = author
+        self.description = description
+        self.imageURL = imageURL
+        self.createdTime = createdTime
+        self.currentUserLikesThisPost = currentUserLikesThisPost
+        self.likedByCount = likedByCount
+        
+    }
+}
